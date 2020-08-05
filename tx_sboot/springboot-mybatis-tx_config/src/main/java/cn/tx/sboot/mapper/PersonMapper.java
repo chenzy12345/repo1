5f8dc280acbe872802486b56ef016a5ad742d29a
chenzhiyuan <1280585860@qq.com>
2020-08-05 19:58:54 +0800

    添加hello4

---
+++ tx_sboot/springboot-mybatis-tx_config/src/main/java/cn/tx/sboot/mapper/PersonMapper.java
@@ -0,0 +1,17 @@
+package cn.tx.sboot.mapper;
+
+import cn.tx.sboot.model.Person;
+
+import java.util.List;
+
+//@Mapper
+public interface PersonMapper {
+
+    public Person selectById(int pid);
+
+    public List<Person> selectAll();
+
+    public void insert(Person p);
+
+    public void delete(int pid);
+}
